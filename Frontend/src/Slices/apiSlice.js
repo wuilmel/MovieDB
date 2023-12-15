95f45afd8bdebd81a570a50fadd9650e621e537f
wuilmel <98865010+wuilmel@users.noreply.github.com>
2023-12-15 14:51:50 -0500

    Delete Frontend directory

--- Frontend/src/Slices/apiSlice.js
+++
@@ -1,9 +0,0 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-const baseQuery = fetchBaseQuery ({ baseUrl: 'http://localhost:8000'});
-
-export const apiSlice = createApi ({
-    baseQuery,
-    tagTypes: ['User'],
-    endpoints: (builder) => ({}),
-});
