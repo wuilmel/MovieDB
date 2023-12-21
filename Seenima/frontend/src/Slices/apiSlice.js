f54898d9ff0050516b109da036665cb61674de6a
wuilmel <98865010+wuilmel@users.noreply.github.com>
2023-12-20 23:51:17 -0500

    Update apiSlice.js

--- Seenima/frontend/src/Slices/apiSlice.js
+++ Seenima/frontend/src/Slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: ''});
+const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:8000'});
 
 export const apiSlice = createApi({
   baseQuery,
