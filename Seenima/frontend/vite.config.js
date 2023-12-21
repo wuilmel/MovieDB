c8b0c57564e7a5f898acd2d9456e0a363cc0f6f9
wuilmel <98865010+wuilmel@users.noreply.github.com>
2023-12-21 01:49:46 -0500

    Update vite.config.js

--- Seenima/frontend/vite.config.js
+++ Seenima/frontend/vite.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       '/api': {
-        target:'https://seenima-n3v1.onrender.com',
+        target:'http://localhost:8000',
         changeOrigin: true,
         secure: false,
       },
