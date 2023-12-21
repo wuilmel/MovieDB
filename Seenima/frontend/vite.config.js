8d056f2376e11277e1fdeda69c0fb5b99ebdbafd
wuilmel <98865010+wuilmel@users.noreply.github.com>
2023-12-21 01:25:43 -0500

    Update vite.config.js

--- Seenima/frontend/vite.config.js
+++ Seenima/frontend/vite.config.js
@@ -1,3 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 4000,
+//     proxy: {
+//       '/api': {
+//         target: 'https://seenima-n3v1.onrender.com',
+//         changeOrigin: true,
+//         secure: false
+//       }
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,10 +26,11 @@ export default defineConfig({
     port: 4000,
     proxy: {
       '/api': {
-        target: 'https://seenima-n3v1.onrender.com',
+        target: process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://seenima-n3v1.onrender.com',
         changeOrigin: true,
-        secure: false
-      }
+        secure: false,
+      },
     }
   }
 })
+
