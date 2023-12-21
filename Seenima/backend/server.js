f35ac3ed6584d7b1a89c1e2fec2010c751027075
wuilmel <98865010+wuilmel@users.noreply.github.com>
2023-12-20 23:41:15 -0500

    Update server.js

--- Seenima/backend/server.js
+++ Seenima/backend/server.js
@@ -6,6 +6,7 @@ import cookieParse from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
+import path from 'path';
 
 const port = process.env.PORT || 6000;
  
@@ -115,6 +116,19 @@ app.get('/search/:text', (req, res) => {
   });
 
 app.use('/api/users', userRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.get('/', (req,res) => res.send('Server is ready'));
 
